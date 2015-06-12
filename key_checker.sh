b56b126fd01ade6593722c541d7bd7732bbcf1fa
Artis Abolts <artis.abolts@gmail.com>
2015-06-12 21:54:14 +0300

    Short description and some imrovements

--- key_checker.sh
+++ key_checker.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+###############################################################################
+# Script using which can find and verify public key length on Linix and MAC OS
+#
+# Find and check keys under current and it child directories:
+#    /key_checker.sh
+#
+# Find and check keys under passed directory and it child directories:
+#    SEARCH_FROM=/home/foo/ /key_checker.sh
+#
+# Extra variables
+#    MIN_KEY_SIZE   - can pass min key size. Default 2048
+#    DEBUG          - run script in debug mode
+#
+###############################################################################
+
 if [ -n "$DEBUG" ]; then
   set -x
 fi
@@ -7,6 +23,7 @@ min_key_size=${MIN_KEY_SIZE:="2048"}
 search_from=${SEARCH_FROM:=${PWD}}
 # Make tempfile. Support Linux and OSX
 tmp_file=$(mktemp 2>/dev/null || mktemp -t key)
+number_regexp='^[0-9]+$'
 
 echo "Min key size: $min_key_size"
 
@@ -19,6 +36,7 @@ for key_file in "${key_files[@]}"; do
 
   # To make sure the last line is always read - whether newline-terminated or not
   while read line || [[ -n $line ]]; do
+
     echo $line > "$tmp_file"
     check=$(ssh-keygen -lf "$tmp_file")
 
@@ -26,6 +44,12 @@ for key_file in "${key_files[@]}"; do
     [[ $check =~ ^([^[:space:]]+)(.*) ]]
     key_size=${BASH_REMATCH[1]}
     rest_check_data=${BASH_REMATCH[2]}
+
+    # Skip if not number
+    if ! [[ $key_size =~ $number_regexp ]] ; then
+      echo -e "  \e[33mSkip line. Seams is not a key\e[0m $line"
+      continue
+    fi
 
     # Get key parts
     [[ $line =~ ^([^[:space:]]+)[[:space:]]+([^[:space:]]+)?[[:space:]]+?([^[:space:]]+)? ]];
@@ -42,7 +66,8 @@ for key_file in "${key_files[@]}"; do
       else
         echo "    $key"
       fi
-
+    elif [ "$key_size" -gt "$min_key_size" ]; then
+      echo -e "  Key size: \e[32m$key_size\e[0m"
     else
       echo "  Key size: $key_size"
     fi
